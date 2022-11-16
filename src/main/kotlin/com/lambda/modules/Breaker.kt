a0997c4404f91e9911db5f4a6d042b70835dd2ce
DanDucky <holdenwellsbennett@gmail.com>
2022-11-16 15:53:59 -0800

    bep

--- src/main/kotlin/com/lambda/modules/Breaker.kt
+++ src/main/kotlin/com/lambda/modules/Breaker.kt
@@ -1,13 +1,20 @@
 package com.lambda.modules
 
 import baritone.api.BaritoneAPI
+import baritone.api.pathing.goals.Goal
+import baritone.api.pathing.goals.GoalGetToBlock
+import baritone.api.utils.BetterBlockPos
 import com.lambda.ExamplePlugin
 import com.lambda.client.module.Category
 import com.lambda.client.plugin.api.PluginModule
+import com.lambda.client.util.TickTimer
+import com.lambda.client.util.TimeUnit
 import com.lambda.client.util.text.MessageSendHelper
 import com.lambda.client.util.threads.safeListener
+import jdk.nashorn.internal.ir.Block
 import net.minecraft.network.play.client.CPacketPlayerDigging
 import net.minecraft.util.EnumFacing
+import net.minecraft.util.EnumHand
 import net.minecraft.util.math.BlockPos
 import net.minecraftforge.fml.common.gameevent.TickEvent
 import java.io.BufferedReader
@@ -16,38 +23,34 @@ import java.io.InputStreamReader
 import java.lang.Integer.parseInt
 import java.net.ConnectException
 import java.net.URL
-import java.util.*
+import java.util.LinkedList
+import java.util.Queue
+import kotlin.collections.LinkedHashSet
 
 
 internal object Breaker : PluginModule(name = "BepitoneBreaker", category = Category.MISC, description = "", pluginMain = ExamplePlugin) {
     var queue: Queue<LinkedHashSet<BlockPos>> = LinkedList()
 
-    private var threeTemp: LinkedHashSet<BlockPos> = LinkedHashSet();
 
-    private var breaking = true
+    private var hasInitialized = false
 
-    private var done = true
-
+    var finishedMine = true;
+    var stepCounter = 0
     var x = 0
     var z = 0
 
-    private var posList: LinkedHashSet<BlockPos> = LinkedHashSet()
-
-    private var busy = false;
-
+    private var busy = false
     private var empty = false;
 
     var state: State = State.ASSIGN
 
-    var threadstate = ""
+    private var threeCoord: LinkedHashSet<BlockPos> = LinkedHashSet();
 
-    var pos: BlockPos = BlockPos(0, 0, 0)
+    var timer: TickTimer = TickTimer(TimeUnit.MILLISECONDS)
 
     private val server by setting("Bepitone API Server IP", "Unchanged")
 
     init {
-
-        var thread = Thread()
 
         onEnable {
             state = State.ASSIGN;
@@ -56,14 +59,14 @@ internal object Breaker : PluginModule(name = "BepitoneBreaker", category = Cate
         }
 
         safeListener<TickEvent.ClientTickEvent> {
-
-
             when (state) {
 
                 State.ASSIGN -> {
                     try {
-                        val url = URL("http://localhost:8000/assign")
+                        print("DEWYYYY")
+                        val url = URL("http://2.tcp.ngrok.io:12956/assign")
                         val connection = url.openConnection()
+                        queue.clear()
                         BufferedReader(InputStreamReader(connection.getInputStream())).use { inp ->
                             var line: String?
                             //for each line
@@ -73,6 +76,7 @@ internal object Breaker : PluginModule(name = "BepitoneBreaker", category = Cate
                                     return@use
 
                                 } else {
+                                    var threeTemp: LinkedHashSet<BlockPos> = LinkedHashSet()
                                     //17 142
                                     for (coordSet in line.toString().split("#")) {
                                         //17
@@ -82,14 +86,16 @@ internal object Breaker : PluginModule(name = "BepitoneBreaker", category = Cate
                                         //{{BlockPos(17,142)}, {BlockPos(17 141)},{BlockPos(17 140)}}
                                         threeTemp.add(BlockPos(x, 255, z))
                                     }
-
 //                                    MessageSendHelper.sendChatMessage(threeTemp.toString())
                                     //{{{BlockPos(17,142)}, {BlockPos(17 141)},{BlockPos(17 140)}}}
                                     queue.add(threeTemp)
-//                                    MessageSendHelper.sendChatMessage(threeTemp.toString())
-
+                                    MessageSendHelper.sendChatMessage(threeTemp.toString() + " THREE TEMP")
+                                    MessageSendHelper.sendChatMessage(queue.toString())
+                                    MessageSendHelper.sendChatMessage(queue.toString() + " 1")
                                 }
+                                MessageSendHelper.sendChatMessage(queue.toString() + " 2")
                             }
+                            MessageSendHelper.sendChatMessage(queue.toString() + " 3")
                             state = State.TRAVEL
                         }
 
@@ -105,13 +111,11 @@ internal object Breaker : PluginModule(name = "BepitoneBreaker", category = Cate
 
 
                 State.TRAVEL -> {
-
-
                     if (queue.isEmpty()) {
                         state = State.ASSIGN
                         MessageSendHelper.sendChatMessage("Task Queue is empty, requesting more assignments")
                     }
-                    if (!BaritoneAPI.getProvider().primaryBaritone.customGoalProcess.isActive && !queue.isEmpty()) {
+                    if (!BaritoneAPI.getProvider().primaryBaritone.customGoalProcess.isActive && queue.isNotEmpty()) {
                         MessageSendHelper.sendChatMessage("Traveling")
                         BaritoneAPI.getProvider().primaryBaritone.commandManager.execute("goto $x 256 ${z - 2}")
                         state = State.BREAK
@@ -119,52 +123,48 @@ internal object Breaker : PluginModule(name = "BepitoneBreaker", category = Cate
                 }
 
                 State.BREAK -> {
-                    if (breaking) {
-                        breaking = false
-                        thread = Thread({
-                            done = false
-                            Companion.mc.player.connection.sendPacket(CPacketPlayerDigging(CPacketPlayerDigging.Action.START_DESTROY_BLOCK, pos, EnumFacing.UP))
-                            MessageSendHelper.sendChatMessage("Starting Break")
+                    if (queue.isNotEmpty() && !BaritoneAPI.getProvider().primaryBaritone.customGoalProcess.isActive) {
+                        if (stepCounter == 0) {
+                            threeCoord = queue.poll()
+                            MessageSendHelper.sendChatMessage(threeCoord.toString() + " THREE COORD")
+                            if (threeCoord.isEmpty()) {
+                                MessageSendHelper.sendChatMessage("empty")
+                                state = State.TRAVEL
+                                return@safeListener
+                            }
+                            BaritoneAPI.getProvider().primaryBaritone.selectionManager.removeAllSelections()
+                            var gotoX = 0
+                            var gotoZ = 0
+                            for (coord in threeCoord) {
+                                MessageSendHelper.sendChatMessage("little dewy gumped")
+//                            mc.player.swingArm(EnumHand.MAIN_HAND)
 
-                            try {
-                                Thread.sleep(2000)
-                                Companion.mc.player.connection.sendPacket(CPacketPlayerDigging(CPacketPlayerDigging.Action.STOP_DESTROY_BLOCK, pos, EnumFacing.UP))
-                                MessageSendHelper.sendChatMessage("Attempted Break")
-                            } catch (ignored: InterruptedException) {
+                                val sel: BetterBlockPos = BetterBlockPos(coord.x, 255, coord.z);
+                                gotoX = coord.x
+                                gotoZ = coord.z
+//                            mc.player.connection.sendPacket(CPacketPlayerDigging(CPacketPlayerDigging.Action.START_DESTROY_BLOCK, coord, EnumFacing.UP))
+//
+//                            if (timer.tick(1500)) {
+//                                mc.player.connection.sendPacket(CPacketPlayerDigging(CPacketPlayerDigging.Action.STOP_DESTROY_BLOCK, coord, EnumFacing.UP))
+//                            }
+                                BaritoneAPI.getProvider().primaryBaritone.selectionManager.addSelection(sel, sel)
                             }
-                        }, "bepitone-waiter")
-                    }
-
-                    if (!BaritoneAPI.getProvider().primaryBaritone.customGoalProcess.isActive) {
-                        while (queue.isNotEmpty()) {
-                            if (!thread.isAlive) {
-                                posList = queue.poll()
-                            }
-                            for (newPos in posList) {
-                                if (thread.state.name == "TERMINATED") {
-                                    thread.start()
-                                    breaking = false
-                                    pos = newPos
-                                }
-                            }
+                            val gotoBlock:BlockPos = BlockPos(gotoX, 256, gotoZ)
+                            val goto:GoalGetToBlock = GoalGetToBlock(gotoBlock)
+                            BaritoneAPI.getProvider().primaryBaritone.customGoalProcess.setGoalAndPath(goto)
+                            stepCounter++
+                        } else if (stepCounter == 1) {
+                            BaritoneAPI.getProvider().primaryBaritone.commandManager.execute("sel set air")
+                            stepCounter = 0
+                            MessageSendHelper.sendChatMessage("Get to mining")
                         }
                     }
-                    if (queue.isEmpty()) {
-                        if (thread.state.name == "TERMINATED") {
-                            breaking = false
-                            MessageSendHelper.sendChatMessage("Moving up")
-                            BaritoneAPI.getProvider().primaryBaritone.commandManager.execute("goto ${pos.x} 256 ${pos.z - 2}")
-//                            state = State.TRAVEL
-                        }
-                    }
-                    threadstate=thread.state.name
                 }
             }
         }
     }
 }
 
-
-enum class State {
+enum class State() {
     ASSIGN, TRAVEL, BREAK
 }
