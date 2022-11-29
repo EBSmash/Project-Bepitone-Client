223a4d4137d82dd2864dcf98d3bc084a6bc7e819
DanDucky <holdenwellsbennett@gmail.com>
2022-11-29 10:39:33 -0800

    Maybe a fix for the disconnect handler
    uwu

--- src/main/kotlin/com/lambda/modules/Breaker.kt
+++ src/main/kotlin/com/lambda/modules/Breaker.kt
@@ -13,11 +13,13 @@ import com.lambda.client.util.Wrapper.world
 import com.lambda.client.util.text.MessageSendHelper
 import com.lambda.client.util.threads.safeListener
 import net.minecraft.block.BlockAir
+import net.minecraft.block.BlockObsidian
 import net.minecraft.client.Minecraft
 import net.minecraft.init.Blocks
 import net.minecraft.util.math.BlockPos
 import net.minecraftforge.fml.common.gameevent.PlayerEvent
 import net.minecraftforge.fml.common.gameevent.TickEvent
+import net.minecraftforge.fml.common.network.FMLNetworkEvent.ClientDisconnectionFromServerEvent
 import java.io.BufferedReader
 import java.io.IOException
 import java.io.InputStreamReader
@@ -36,7 +38,7 @@ internal object Breaker : PluginModule(name = "BepitoneBreaker", category = Cate
     var blocks_broken = 0
 
     var delay = 0
-
+    var delayReconnect = 0
     var breakCounter = 0
     var x = 0
     var z = 0
@@ -54,8 +56,8 @@ internal object Breaker : PluginModule(name = "BepitoneBreaker", category = Cate
 
     var username = "";
 
-    private val url by setting("Server IP", "3.22.30.40")
-    private val port by setting("Server Port", "19439")
+    private val url by setting("Server IP", "2.tcp.ngrok.io")
+    private val port by setting("Server Port", "10696")
 
     var id = "0";
 
@@ -97,6 +99,29 @@ internal object Breaker : PluginModule(name = "BepitoneBreaker", category = Cate
         }
 
         safeListener<TickEvent.ClientTickEvent> {
+
+            // Disconnect
+            val mc = Minecraft.getMinecraft()
+            val data = mc.currentServerData
+            if (data == null && state != State.QUEUE) {
+                state = State.QUEUE
+                try {
+                    println("Running bepatone shutdown hook")
+
+//                    println(mc.player.posY.toInt().toString())
+                    println(exitCoord)
+
+                    val url = URL("http://$url:$port/fail/${Breaker.file}/${Breaker.x}/256/${Breaker.z}/${username}")
+
+                    with(url.openConnection() as HttpURLConnection) {
+                        requestMethod = "GET"  // optional default is GET
+                        println("\nSent 'GET' request to URL : $url:$port; Response Code : $responseCode")
+                    }
+                } catch (e: Exception) {
+                    println("Running bepatone shutdown hook failed")
+                }
+            }
+
             when (state) {
 
                 State.ASSIGN -> {
@@ -191,7 +216,7 @@ internal object Breaker : PluginModule(name = "BepitoneBreaker", category = Cate
                                 BaritoneAPI.getProvider().primaryBaritone.selectionManager.addSelection(sel, sel)
                                 z = coord.z
                                 x = coord.x
-                                if (mc.world.getBlockState(BlockPos(x,255,z)).block !is BlockAir) {
+                                if (mc.world.getBlockState(BlockPos(x + xOffset,255,z + zOffset)).block !is BlockAir) {
                                     needToMine = true
                                 }
                             }
@@ -200,41 +225,47 @@ internal object Breaker : PluginModule(name = "BepitoneBreaker", category = Cate
                                 BaritoneAPI.getProvider().primaryBaritone.selectionManager.addSelection(sel, sel)
                                 z = coord.z
                                 x = coord.x
-                                if (mc.world.getBlockState(BlockPos(x,255,z)).block !is BlockAir) {
+                                if (mc.world.getBlockState(BlockPos(x + xOffset,255,z + zOffset)).block !is BlockAir) {
+                                    MessageSendHelper.sendChatMessage("bep")
                                     needToMine = true
                                 }
                             }
+                            MessageSendHelper.sendChatMessage(needToMine.toString())
+                            breakCounter++
                             if (needToMine) {
                                 if (mc.world.getBlockState(BlockPos(2 + (xOffset + file * 5), 255 ,z + zOffset + negPosCheck(file))) !is BlockAir) { // thanks leijurv papi
                                     BaritoneAPI.getProvider().primaryBaritone.commandManager.execute("goto ${2 + (xOffset + file * 5)} 256 ${z + zOffset + negPosCheck(file)}")
                                 }
+                            } else {
+                                breakCounter = 0
                             }
-                            breakCounter++
                         } else if (breakCounter == 1) {
                             BaritoneAPI.getProvider().primaryBaritone.commandManager.execute("sel set air")
+                            breakCounter++
+                        } else if (breakCounter == 2 && delay != 22) {
+                            delay++
+                        } else {
+                            delay = 0
                             breakCounter = 0
+                            BaritoneAPI.getProvider().primaryBaritone.commandManager.execute("sel set air")
                         }
-//                        } else if (breakCounter == 2 && delay != 22) {
-//                            delay++
-//                        } else {
-//                            delay = 0
-//                            breakCounter = 0
-//                            BaritoneAPI.getProvider().primaryBaritone.commandManager.execute("sel set air")
-//                        }
                     }
                 }
                 State.QUEUE -> {
                     // await joining server
-                    val mc = Minecraft.getMinecraft()
-                    val data = mc.currentServerData
                     if (data != null) {
-                        if (data.serverIP.lowercase().equals("connect.2b2t.org") && mc.player.dimension == 0) {
+                        if (mc.player.dimension == 0 && delayReconnect != 20) {
+                            delayReconnect++
+                        } else if (mc.player.dimension == 0 && delayReconnect == 20) {
                             state = State.ASSIGN
+                            delayReconnect = 0
                         }
                     }
                 }
             }
-            if (player.posY < 200) { // if player falls
+
+            if (player.posY < 200 && (state == State.BREAK || state == State.TRAVEL)) { // if player falls
+                MessageSendHelper.sendChatMessage("dis you paper?")
                 try {
                     println("Running bepatone shutdown hook")
                     disable() // does this run the disable shutdown hook or not? idk if it's been working correctly
@@ -243,30 +274,12 @@ internal object Breaker : PluginModule(name = "BepitoneBreaker", category = Cate
                 }
             }
         }
-        safeListener<ConnectionEvent.Disconnect> {
-            state = State.QUEUE
-            try {
-                println("Running bepatone shutdown hook")
-
-                println(mc.player.posY.toInt().toString())
-                println(exitCoord)
-
-                val url = URL("http://$url:$port/fail/${Breaker.file}/${Breaker.x}/${mc.player.posY.toInt()}/${Breaker.z}/${username}")
-
-                with(url.openConnection() as HttpURLConnection) {
-                    requestMethod = "GET"  // optional default is GET
-                    println("\nSent 'GET' request to URL : $url:$port; Response Code : $responseCode")
-                }
-            } catch (e: Exception) {
-                println("Running bepatone shutdown hook failed")
-            }
-        }
-
         onDisable {
             try {
                 println("Running bepatone shutdown hook")
 
                 println(mc.player.posY.toInt().toString())
+                BaritoneAPI.getProvider().primaryBaritone.commandManager.execute("stop")
 
                 val url = URL("http://$url:$port/fail/${Breaker.file}/${Breaker.x}/256/${Breaker.z}/${username}")
 
